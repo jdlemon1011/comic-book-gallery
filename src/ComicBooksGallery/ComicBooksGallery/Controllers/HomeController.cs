0910b9f233f59852ae3e893621b1fe0a72c4684b
jdlemon <jlemon@paysimple.com>
2022-03-15 06:04:51 -0600

    Restarted....again

---
+++ src/ComicBooksGallery/ComicBooksGallery/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ComicBooksGallery.Controllers
+{
+	public class ComicBooksController : Controller
+	{
+		public IActionResult Detail()
+		{
+			return View();
+			
+		}
+	}
+}
